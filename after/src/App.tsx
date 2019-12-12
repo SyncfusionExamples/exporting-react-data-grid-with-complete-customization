70ef5dd7337473a75617dbf58c76f653e70797c6
Karthikeyan <karthikeyan.live@hotmail.com>
2019-12-12 12:05:27 +0530

    Sample modified as in video

--- after/src/App.tsx
+++ after/src/App.tsx
@@ -4,48 +4,24 @@ import {
   ColumnDirective,
   ColumnsDirective,
   Page,
+  Inject,
   Toolbar,
-  Grid,
   PdfExport,
-  ExcelExport,
-  Inject
+  Grid,
+  ExcelExport
 } from '@syncfusion/ej2-react-grids';
 import data from './dataSource.json';
 import './App.css';
 
 const App: React.FC = () => {
-  let grid: Grid | null = null;
 
+  let grid: Grid | null;
   const toolbarClick = (args: any) => {
     if (grid) {
       if (args.item.id.includes('pdfexport')) {
         grid.pdfExport({
-          fileName: 'Data_Grid_Export.pdf',
+          fileName: 'invoice.pdf',
           exportType: 'CurrentPage',
-          header: {
-            contents: [
-              {
-                position: { x: 0, y: 50 },
-                style: { textBrushColor: '#000000', fontSize: 13 },
-                type: 'Text',
-                value: "Northwind Traders"
-              }
-            ],
-            fromTop: 0,
-            height: 130
-          },
-          footer: {
-            contents: [
-              {
-                position: { x: 0, y: 50 },
-                style: { textBrushColor: '#000000', fontSize: 13 },
-                type: 'Text',
-                value: "Thank you for your business!"
-              }
-            ],
-            fromBottom: 130,
-            height: 130
-          },
           theme: {
             header: {
               bold: true,
@@ -58,51 +34,77 @@ const App: React.FC = () => {
               fontName: 'Calibri',
               fontSize: 8
             }
+          },
+          header: {
+            fromTop: 0,
+            height: 130,
+            contents: [{
+              type: 'Text',
+              value: 'Northwind Traders',
+              position: { x: 0, y: 50 },
+              style: { textBrushColor: '#000000', fontSize: 13 }
+            }]
+          },
+          footer: {
+            contents: [{
+              type: 'Text',
+              value: 'Thank you for your Business',
+              position: { x: 0, y: 50 },
+              style: { textBrushColor: '#000000', fontSize: 13 }
+            }],
+            fromBottom: 130,
+            height: 130
           }
         });
       } else if (args.item.id.includes('excelexport')) {
         grid.excelExport({
-          fileName: "Data_Grid_Export.xlsx",
+          fileName: 'invoice.xlsx',
+          exportType: 'CurrentPage',
+          theme: {
+            header: {
+              bold: true,
+              fontColor: '#00ff00',
+              fontName: 'Calibri',
+              fontSize: 10
+            },
+            record: {
+              fontColor: '#0000ff',
+              fontName: 'Calibri',
+              fontSize: 8
+            }
+          },
           header: {
             headerRows: 1,
             rows: [
-                { cells: [{ colSpan: 5, value: "Northwind Traders", style: { fontColor: '#C67878', fontSize: 20, hAlign: 'Center', bold: true, } }] },
+              {
+                cells: [{
+                  colSpan: 4,
+                  value: "Northwind Traders",
+                  style: { fontColor: '#C67878', fontSize: 20, hAlign: 'Center', bold: true, }
+                }]
+              }
             ]
-        },
-        footer: {
-          footerRows: 1,
-          rows: [
-              { cells: [{ colSpan: 5, value: "Thank you for your business!", style: { hAlign: 'Center', bold: true } }] },
-          ]
-        },
-        exportType: 'CurrentPage',
-        theme: {
-          header: {
-            bold: true,
-            fontColor: '#00ff00',
-            fontName: 'Calibri',
-            fontSize: 10
           },
-          record: {
-            fontColor: '#0000ff',
-            fontName: 'Calibri',
-            fontSize: 8
+          footer: {
+            footerRows: 1,
+            rows: [
+              { cells: [{ colSpan: 5, value: "Thank you for your business!", style: { hAlign: 'Center', bold: true } }] },
+            ]
+
           }
-        }
         });
       }
     }
   }
-
   return (
     <div style={{ margin: '10%', marginTop: '5%' }}>
       <GridComponent dataSource={data}
-        allowPaging={true}
-        allowPdfExport={true}
-        allowExcelExport={true}
-        toolbar={['PdfExport', 'ExcelExport']}
-        toolbarClick={toolbarClick}
         ref={g => grid = g}
+        allowPaging={true}
+        toolbar={['PdfExport', 'ExcelExport']}
+        allowPdfExport={true}
+        allowExcelExport
+        toolbarClick={toolbarClick}
       >
         <ColumnsDirective>
           <ColumnDirective field='OrderID' headerText='Invoice ID' textAlign='Right' width='100' isPrimaryKey={true} />
@@ -111,7 +113,7 @@ const App: React.FC = () => {
           <ColumnDirective field='ShipName' headerText='Ship Name' />
           <ColumnDirective field='Freight' textAlign='Right' format='C2' width='100' />
         </ColumnsDirective>
-        <Inject services={[Page, PdfExport, Toolbar, ExcelExport]} />
+        <Inject services={[Page, Toolbar, PdfExport, ExcelExport]} />
       </GridComponent>
     </div>
   );
